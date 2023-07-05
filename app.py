aa6f2843b7ee55382dcefdee78abb3146fbed797
George 228 <maxkostenko2001@mail.ru>
2023-07-05 20:30:47 +0300

    added gunicorn

--- app.py
+++
@@ -1,8 +0,0 @@
-from waitress import serve
-
-from routes import app
-
-
-if __name__ == "__main__":
-    # app.run(host='localhost', port=5000, debug=True)
-    serve(app, host='localhost', port=5000)
