33de9eaa63c251286d8ad26a704d22033e4c4a76
George 228 <maxkostenko2001@mail.ru>
2023-07-05 20:32:07 +0300

    refactoring: vars changed to env vars

--- flask_app/app_config.py
+++ flask_app/app_config.py
@@ -1,15 +1,16 @@
+import os
 from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from datetime import timedelta
 
-SESSION_TIME = timedelta(minutes=30)
+SESSION_TIME = timedelta(minutes=int(os.environ['SESSION_TIME_MINUTES']))
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///survey.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'an(v$g+fv=&5%bp*-nt)rkv6y1^vi)v%ew_sm_p8sqiseyjrh%'
-app.config['SQLALCHEMY_ECHO'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['SQLALCHEMY_DATABASE_URI']
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = os.environ['SQLALCHEMY_TRACK_MODIFICATIONS']
+app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
+app.config['SQLALCHEMY_ECHO'] = os.environ['SQLALCHEMY_ECHO']
 
 db = SQLAlchemy()
 db.init_app(app)
